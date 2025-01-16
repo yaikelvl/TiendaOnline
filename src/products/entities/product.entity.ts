5d18ebd14059ce2e7784c32ab6405087b938f217
Yaikel Valdes Luis <yaikelvl@gmail.com>
2025-01-16 16:46:59 -0500

    Create Product make

---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,24 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Unit } from '../enums/unit.enum';
+
+@Entity()
+export class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column('text', { unique: true })
+  title: string;
+
+  @Column('numeric', { default: 0 })
+  price: number;
+
+  @Column('text', { nullable: true })
+  description: string;
+
+  @Column('text', { unique: true })
+  slug: string;
+
+  @Column('text')
+  unit: Unit;
+
+}
