156a6e0986116747637636abaf8616f5957cf4c5
Yaikel Valdes Luis <yaikelvl@gmail.com>
2025-01-16 09:23:52 -0500

    Instalacion de paquetes y configuracion de TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT), // convert string to number tambien se puede usar de la sigiente forma +process.env.DB_PORT.
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
