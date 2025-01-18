c069eff80fc2db122ce373d2c8a727f86042d876
Yaikel Valdes Luis <yaikelvl@gmail.com>
2025-01-18 13:51:27 -0500

    Crud actualizado hasta la actualizacion de imagenes

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -38,7 +38,9 @@ export class Product {
   @OneToMany(
     () => ProductImage,
     (productImage) => productImage.product,
-    { cascade: true },
+    { cascade: true,
+      eager: true
+     },
   )
   images?: ProductImage[];
 
